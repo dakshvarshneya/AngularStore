08433b674b398d0776ec01c5a480647ba769d3ba
Daksh Varshneya <dakshvarshneya5@gmail.com>
2018-02-09 18:09:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@
 	});
 			
 			var gems =[ 
-				{name: '1. Dodecahedron',price: 2.95,description: 'Description Of First Gem',canPurchase: true, image:"logo-preview.png",
+				{name: '1. Dodecahedron',price: 2.95,description: 'Description Of First Gem',canPurchase: true, image:"gem1.jpg",
 				
 				},
-				{name: '2. Pentagonal Gem',price: 5.95,description: 'Description Of Second Gem',canPurchase: true, image:"logo-preview.png"},
+				{name: '2. Pentagonal Gem',price: 5.95,description: 'Description Of Second Gem',canPurchase: true, image:"gem2.jpg"},
 			];
 })();
